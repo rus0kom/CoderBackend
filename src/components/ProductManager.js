bab98b870f1cf9a3c95f3d485d2890e906f60963
Ivan Komissarov <i.komissarov1@gmail.com>
2023-09-06 19:10:51 -0300

    primer entrega proyecto final

--- src/components/ProductManager.js
+++ src/components/ProductManager.js
@@ -1,94 +1,64 @@
-import {promises as fs, writeFile} from "fs"
+import { promises as fs } from "fs";
+import { nanoid } from "nanoid";
 
-export default class ProductManager {
-    constructor(){
-        this.patch = "./productos.txt"
-        this.products = []
+class ProductManager {
+    constructor () {
+        this.path = "./src/models/products.json"
     }
-
-    static id = 0
-
-    addProduct = async (title, description, price, imagen, code, stock) => {
-
-        ProductManager.id++;
-
-        let newProduct = {
-            title,
-            description,
-            price,
-            imagen,
-            code,
-            stock,
-            id: ProductManager.id
-        };
-
-        this.products.push(newProduct)
-
-        console.log(newProduct)
-
-        await fs.writeFile(this.patch, JSON.stringify(this.products)); 
-    };
 
     readProducts = async () => {
-        let respuesta = await fs.readFile(this.patch, "utf-8");
-        return JSON.parse(respuesta)
+        let products = await fs.readFile(this.path, "utf-8");
+        return JSON.parse(products);
     }
 
-    getProducts = async () => {
-        let respuesta2 = await this.readProducts()
-        return console.log(respuesta2)
+    writeProducts = async (product) => {
+        await fs.writeFile(this.path, JSON.stringify(product));
+    } 
+
+    exist = async (id) => {
+        let products = await this.readProducts();
+        return products.find(prod => prod.id === id)
     }
 
-    getProductById = async (id) => {
-        let respuesta3 = await this.readProducts()
-        if(!respuesta3.find(product => product.id === id)){
-            console.log("Product not found")
-        } else {
-            console.log(respuesta3.find(product => product.id === id))
-        }
+    addProducts = async (product) => {
+        let productsOld = await this.readProducts()
+        product.id = nanoid()
+        let productAll = [...productsOld, product];
+        await this.writeProducts(productAll);
+        return "Producto Agregado";
+    };
 
-     };
+    getProducts = async () =>{
+        return await this.readProducts()
+    };
 
-    deleteProductById = async (id) => {
-        let respuesta3 = await this.readProducts();
-        let productFilter = respuesta3.filter(products => products.id != id)
-        await fs.writeFile(this.patch, JSON.stringify(productFilter));
+    getProductsById = async (id) =>{
+        let productById = await this.exist(id);
+        if(!productById) return "Producto no encontrado"
+        return productById
+    };
 
-        console.log("Product Deleted")
-
-     };
-
-    updateProducts = async ({id, ...producto}) => {
-        await this.deleteProductById(id);
+    updateProducts = async (id, product) => {
+        let productById = await this.exist(id);
+        if(!productById) return "Producto no encontrado"
+        await this.deleteProducts(id)
         let productOld = await this.readProducts();
+        let products = [{...product, id : id }, ...productOld]
+        await this.writeProducts(products)
+        return "Producto actualizado"
+    }
 
+    deleteProducts = async (id) => {
+        let products = await this.readProducts();
+        let existProducts = products.some(prod => prod.id === id)
+        if (existProducts) {
+        let filterProducts = products.filter(prod => prod.id != id)
+        await this.writeProducts(filterProducts)
+        return "Producto eliminado"
+    }
+    return "No existe el producto a eliminar"
+    }
+};
 
-        let productsModificado = [{id, ...producto}, ...productOld];
-        await fs.writeFile(this.patch, JSON.stringify(productsModificado));
-     };
-
-}
-
-//const productos = new ProductManager
-
-// productos.addProduct("titulo1", "description1", 1000, "img1", "code1", 5)
-// productos.addProduct("titulo2", "description2", 2000, "img2", "code2", 10)
-// productos.addProduct("titulo3", "description3", 3000, "img3", "code3", 15)
-
-// productos.getProducts()
-// productos.getProductById(2)
-// productos.getProductById(4)
-
-// productos.deleteProductById(2);
-// productos.updateProducts({
-// title: 'otrotitulo',
-// description: 'otradescription3',
-// price: 3500,
-// imagen: 'otraimg',
-// code: 'otrocode',
-// stock: 555,
-// id: 3
-// })
-
-// productos.getProducts()
+export default ProductManager
 
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import ProductRouter from "./router/product.routes.js";
+import CartRouter from "./router/carts.routes.js";
+
+const app = express();
+const PORT = 8080
+
+app.use(express.json())
+app.use(express.urlencoded({ extended : true }))
+
+app.use("/products", ProductRouter)
+app.use("/cart", CartRouter)
+
+app.listen(PORT, ()=> {
+    console.log(`Servidor express puerto ${PORT}`);
+});
