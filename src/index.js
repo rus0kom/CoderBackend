f2418b056d19f409f30c7eda599bb1108fea5e53
Ivan Komissarov <i.komissarov1@gmail.com>
2023-09-13 16:51:32 -0300

    entrega

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
 import express from "express";
 import ProductRouter from "./router/product.routes.js";
 import CartRouter from "./router/carts.routes.js";
+import { engine } from "express-handlebars";
+import * as path from "path"
+import __dirname from "./utils.js";
+import ProductManager from "./controllers/ProductManager.js";
 
 const app = express();
 const PORT = 8080
+const product = new ProductManager();
 
 app.use(express.json())
 app.use(express.urlencoded({ extended : true }))
+
+//Handlebars
+app.engine("handlebars", engine())
+app.set("view engine", "handlebars")
+app.set("views", path.resolve(__dirname + "/views"))
+
+//Static
+app.use("/", express.static(__dirname + "/public"))
+
+app.get("/", async (req, res) => {
+    let allProducts = await product.getProducts()
+    res.render("home", {
+        title: "Express avanzado | Handlebars",
+        products : allProducts
+    })
+})
+
 
 app.use("/products", ProductRouter)
 app.use("/cart", CartRouter)
--- src/router/carts.routes.js
+++ src/router/carts.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import CartManager from "../components/CartManager.js";
+import CartManager from "../controllers/CartManager.js";
 
 const CartRouter = Router()
 const carts = new CartManager
