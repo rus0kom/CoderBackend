82ecc2a612cc4254d09ff3b2e3be9880ed4b142e
rus0kom <101815935+rus0kom@users.noreply.github.com>
2024-02-05 10:03:46 -0300

    Delete src directory

--- src/routes/user.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import UserController from "../controllers/user.controller.js";
-
-const userRouter = Router();
-
-userRouter.get("/", UserController.getUserById);
-
-export default userRouter;
--- src/utils/getHourAndMinutes.js
+++
@@ -1,5 +0,0 @@
-const getHourAndMinutes = (date) => {
-  return date.getHours() + ":" + date.getMinutes();
-};
-
-export default getHourAndMinutes;
